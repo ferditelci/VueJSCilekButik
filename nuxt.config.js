e0dc2ed378092f9eda4aa7c0d72d8a90fb9381e3
ferditelci <56549064+ferditelci@users.noreply.github.com>
2021-01-03 23:02:18 +0300

    Firebase Projeye Eklendi
    Firebase, nuxt.config dosyası üzerinden modül olarak eklendi.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'cilek-butik-app',
+    title: 'Çilek | Online Alışveriş Sitesi',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon-cilekbutik.png' }
     ]
   },
 
@@ -29,6 +29,34 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyDyqWb1aeh7FSehWbh-NxcoJCbWT4hYz50",
+          authDomain: "cilek-butik.firebaseapp.com",
+          projectId: "cilek-butik",
+          storageBucket: "cilek-butik.appspot.com",
+          messagingSenderId: "839058303191",
+          appId: "1:839058303191:web:21aba96c082cf86c646892"
+        },
+        services: {
+          auth: true,
+          firestore: true,
+          functions: true,
+          storage: true,
+          database: true,
+          messaging: true,
+          performance: true,
+          analytics: true,
+          remoteConfig: true
+        }
+      }
+    ],
+    [
+      '@nuxtjs/axios',
+
+    ]
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
