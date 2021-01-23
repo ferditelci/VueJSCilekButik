30110f9cea56cd99f38b3df2b7a94781168bc2a6
ferditelci <56549064+ferditelci@users.noreply.github.com>
2021-01-23 23:57:35 +0300

    Test with Jest
    Specs oluşturuldu

---
+++ specs/deneme.spec.js
@@ -0,0 +1,20 @@
+import TestComponent from '@/test'
+import List from '@/list'
+
+import { mount } from '@vue/test-utils'
+
+test('mount a vue component', () => {
+  const wrapper = mount(TestComponent,{ 
+    propsData: {
+      value: 'VueSchool'
+    }
+  })
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('ListComponent', () => {
+  const wrapper = mount(List)
+  const movies = wrapper.vm.marvelMovies
+  wrapper.setData({ marvelMovies: [...movies, 'Endgame' ]})
+  expect(wrapper).toMatchSnapshot()
+})
